fbecf911db9b3a5fd5dccc097e9b8ec19c9fdc1d
Graham Laverty <glaverty@gmail.com>
2018-01-27 02:18:35 -0800

    Light tuning for player

--- Tapped for Cash/Assets/Scripts/Player.cs
+++ Tapped for Cash/Assets/Scripts/Player.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class Player : MonoBehaviour {
     private Rigidbody2D mRigidBody2D;
+    private GameObject scanner;
     private float m_scanMoveMultiplier = 0.5f;
     private float m_moveSpeed = 0.2f;
     private float rotateSpeed = 3f;
@@ -12,6 +13,8 @@ public class Player : MonoBehaviour {
     private void Awake()
     {
         mRigidBody2D = GetComponent<Rigidbody2D>();
+        scanner = transform.Find("Dude_scanning").gameObject;
+        scanner.SetActive(false);
     }
 
     void Start () {
@@ -19,12 +22,26 @@ public class Player : MonoBehaviour {
 	}
 	
 	void Update () {
+        // Toggle scan
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            if(isScanning)
+            {
+                isScanning = false;
+            }
+            else
+            {
+                isScanning = true;
+            }
+            scanner.SetActive(isScanning);
+        }
+
         //Rotate
-        if (Input.GetKey(KeyCode.A))
+        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
         {
             gameObject.transform.Rotate(0, 0, (rotateSpeed * (isScanning ? m_scanMoveMultiplier : 1f)));
         }
-        if (Input.GetKey(KeyCode.D))
+        if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
         {
             gameObject.transform.Rotate(0, 0, (-rotateSpeed * (isScanning ? m_scanMoveMultiplier : 1f)));
         }
@@ -34,11 +51,11 @@ public class Player : MonoBehaviour {
 
         Vector3 deltaPos = Vector3.forward;
         deltaPos.z = 0f;
-		if (Input.GetKey(KeyCode.W))
+		if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow))
         {
             deltaPos.y += m_moveSpeed * (isScanning ? m_scanMoveMultiplier : 1f);
         }
-        if (Input.GetKey(KeyCode.S))
+        if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow))
         {
             deltaPos.y -= m_moveSpeed * (isScanning ? m_scanMoveMultiplier : 1f);
         }
