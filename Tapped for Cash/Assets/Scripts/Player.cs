aec5c45fe15051cca20a25eef115587ce73c4c16
CS <hecklerofharbingers@gmail.com>
2018-01-27 03:46:02 -0800

    Cleaned up player script into Update and FixedUpdate for physics
    --HG--
    branch : chelsea

--- Tapped for Cash/Assets/Scripts/Player.cs
+++ Tapped for Cash/Assets/Scripts/Player.cs
@@ -11,6 +11,9 @@ public class Player : MonoBehaviour {
     private float rotateSpeed = 3f;
     private bool isScanning = false;
 
+    private Vector2 deltaPos = Vector2.zero;
+    private float mRotation = 0f;
+
     private void Awake()
     {
         mRigidBody2D = GetComponent<Rigidbody2D>();
@@ -37,64 +40,68 @@ public class Player : MonoBehaviour {
             scanner.SetActive(isScanning);
         }
 
-        //Rotate
-        bool isRotating = false;
+        //Rotation Inputs
+        mRotation = 0f;
         if (Input.GetKey(KeyCode.A))
         {
-            gameObject.transform.Rotate(0, 0, (rotateSpeed * (isScanning ? m_scanMoveMultiplier : 1f)));
-            isRotating = true;
+            mRotation = rotateSpeed * (isScanning ? m_scanMoveMultiplier : 1f);
         }
         if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
         {
-            isRotating = true;
-            gameObject.transform.Rotate(0, 0, (-rotateSpeed * (isScanning ? m_scanMoveMultiplier : 1f)));
+            mRotation = -rotateSpeed * (isScanning ? m_scanMoveMultiplier : 1f);
         }
 
-        //adjust momentum to forward
-        if (isRotating)
-        {
-            Debug.Log(transform.up.normalized);
-            mRigidBody2D.velocity = (transform.up.normalized) * mRigidBody2D.velocity.magnitude;
-        }
         
         
         
-        //Move
-	
-        Vector2 deltaPos = Vector2.zero;
-        bool isStandingStill = true;
+        
+        //Move Inputs
+        deltaPos = Vector2.zero;
    
 		if (Input.GetKey(KeyCode.W))
         {
             deltaPos.y += m_moveSpeed * (isScanning ? m_scanMoveMultiplier : 1f);
-            isStandingStill = false;
         }
         if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow))
         {
             deltaPos.y -= m_moveSpeed * (isScanning ? m_scanMoveMultiplier : 1f);
-            isStandingStill = false;
         }
+
+        
+    }
+
+
+    private void FixedUpdate()
+    {
+        mRigidBody2D.AddRelativeForce(deltaPos);
 
         //cap velocity
         Vector2 newVelocity = mRigidBody2D.velocity;
-        
+
         //gameObject.transform.Translate(deltaPos);
-        mRigidBody2D.AddRelativeForce(deltaPos);
+
         if (newVelocity.magnitude > m_maxSpeed)
         {
             Debug.Log("capped speed");
             newVelocity = newVelocity.normalized * m_maxSpeed;
-            
+
         }
-        if (isStandingStill)
+        if (deltaPos ==Vector2.zero)
         {
             newVelocity *= 0.9f;
         }
         mRigidBody2D.velocity = newVelocity;
 
-        if (!isRotating)
-        {
+        if (mRotation==0)
+        { //stop rotating
             mRigidBody2D.angularVelocity = 0f;
         }
+        else
+        {//adjust momentum to forward
+            Debug.Log(transform.up.normalized);
+            mRigidBody2D.velocity = (transform.up.normalized) * mRigidBody2D.velocity.magnitude;
+        }
     }
+
+
 }
