b8c4b31224245d078a71baeea313a67d924c1edd
CS <hecklerofharbingers@gmail.com>
2018-01-27 02:43:58 -0800

    Improved movement controls
    --HG--
    branch : chelsea

--- Tapped for Cash/Assets/Scripts/Player.cs
+++ Tapped for Cash/Assets/Scripts/Player.cs
@@ -5,7 +5,8 @@ using UnityEngine;
 public class Player : MonoBehaviour {
     private Rigidbody2D mRigidBody2D;
     private float m_scanMoveMultiplier = 0.5f;
-    private float m_moveSpeed = 0.2f;
+    private float m_moveSpeed = 2f;
+    private float m_maxSpeed = 1.5f;
     private float rotateSpeed = 3f;
     private bool isScanning = false;
 
@@ -20,29 +21,60 @@ public class Player : MonoBehaviour {
 	
 	void Update () {
         //Rotate
+        bool isRotating = false;
         if (Input.GetKey(KeyCode.A))
         {
             gameObject.transform.Rotate(0, 0, (rotateSpeed * (isScanning ? m_scanMoveMultiplier : 1f)));
+            isRotating = true;
         }
         if (Input.GetKey(KeyCode.D))
         {
+            isRotating = true;
             gameObject.transform.Rotate(0, 0, (-rotateSpeed * (isScanning ? m_scanMoveMultiplier : 1f)));
         }
-
+        //adjust momentum to forward
+        if (isRotating)
+        {
+            Debug.Log(transform.up.normalized);
+            mRigidBody2D.velocity = (transform.up.normalized) * mRigidBody2D.velocity.magnitude;
+        }
+        
+        
+        
         //Move
-
-
-        Vector3 deltaPos = Vector3.forward;
-        deltaPos.z = 0f;
+        Vector2 deltaPos = Vector2.zero;
+        bool isStandingStill = true;
+   
 		if (Input.GetKey(KeyCode.W))
         {
             deltaPos.y += m_moveSpeed * (isScanning ? m_scanMoveMultiplier : 1f);
+            isStandingStill = false;
         }
         if (Input.GetKey(KeyCode.S))
         {
             deltaPos.y -= m_moveSpeed * (isScanning ? m_scanMoveMultiplier : 1f);
+            isStandingStill = false;
         }
+
+        //cap velocity
+        Vector2 newVelocity = mRigidBody2D.velocity;
         
-        gameObject.transform.Translate(deltaPos);
+        mRigidBody2D.AddRelativeForce(deltaPos);
+        if (newVelocity.magnitude > m_maxSpeed)
+        {
+            Debug.Log("capped speed");
+            newVelocity = newVelocity.normalized * m_maxSpeed;
+            
+        }
+        if (isStandingStill)
+        {
+            newVelocity *= 0.9f;
+        }
+        mRigidBody2D.velocity = newVelocity;
+
+        if (!isRotating)
+        {
+            mRigidBody2D.angularVelocity = 0f;
+        }
     }
 }
