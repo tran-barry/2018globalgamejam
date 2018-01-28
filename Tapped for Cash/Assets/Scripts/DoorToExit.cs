0f1192a51b3589d10ea28b2e87337d4b70bda1a0
CS <hecklerofharbingers@gmail.com>
2018-01-28 02:37:59 -0800

    Art update and started mall exit trigger zone
    --HG--
    branch : chelsea

---
+++ Tapped for Cash/Assets/Scripts/DoorToExit.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorToExit : MonoBehaviour {
+
+    protected void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            //GTFO
+
+        }
+    }
+
+
+}
