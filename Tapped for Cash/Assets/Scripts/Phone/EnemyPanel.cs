80db9dc14c23322ccadc293f04734f7672c2dcb3
maxime <badet.maxime@gmail.com>
2018-01-27 17:03:12 -0800

    no message

---
+++ Tapped for Cash/Assets/Scripts/Phone/EnemyPanel.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EnemyPanel : MonoBehaviour {
+    Image  creditCard;
+    Image  signalLow;
+    Image  signalMedium;
+    Image  signalHigh;
+    Slider progression;
+    Text   cashValue;
+
+    // Use this for initialization
+    void Start ()
+    {
+        creditCard = transform.Find("CreditCard").GetComponent<Image>();
+        signalLow = transform.Find("Signal/Low").GetComponent<Image>();
+        signalMedium = transform.Find("Signal/Medium").GetComponent<Image>();
+        signalHigh = transform.Find("Signal/High").GetComponent<Image>();
+        progression = transform.Find("Progression").GetComponent<Slider>();
+        cashValue = transform.Find("CashValue").GetComponent<Text>();
+    }
+
+    public void Signal (int fSignal)
+    {
+        if (fSignal == 1)
+        {
+            signalLow.color = Color.green;
+            signalMedium.color = Color.grey;
+            signalHigh.color = Color.grey;
+        }
+        else if (fSignal == 2)
+        {
+            signalLow.color = Color.green;
+            signalMedium.color = Color.green;
+            signalHigh.color = Color.grey;
+        }
+        else if (fSignal == 3)
+        {
+            signalLow.color = Color.green;
+            signalMedium.color = Color.green;
+            signalHigh.color = Color.green;
+        }
+        else
+        {
+            signalLow.color = Color.grey;
+            signalMedium.color = Color.grey;
+            signalHigh.color = Color.grey;
+        }
+    }
+
+    public void Progression(int fProgression)
+    {
+        progression.value = fProgression;
+    }
+
+    public void CashValue(int fCashValue)
+    {
+        cashValue.text = "$" + fCashValue;
+    }
+}
